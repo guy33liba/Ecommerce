dc8edc072e0f56c2de923daa3ee6138112b122d3
guy33liba <guy33liba@gmail.com>
2025-03-10 23:44:15 +0200

    כשדג

--- client/src/pages/AddProduct.jsx
+++ client/src/pages/AddProduct.jsx
@@ -21,21 +21,24 @@ const AddProduct = () => {
  return (
   <div>
    <h2>Add New Product</h2>
-   <input
-    type="text"
-    value={product.name}
-    onChange={(e) => setProduct({ ...product, name: e.target.value })}
-   />
-   <input
-    type="number"
-    value={product.price}
-    onChange={(e) => setProduct({ ...product, price: e.target.value })}
-   />
-   <input
-    type="text"
-    value={product.image}
-    onChange={(e) => setProduct({ ...product, image: e.target.value })}
-   />
+   <form onSubmit={handleSubmit}>
+    <input
+     type="text"
+     value={product.name}
+     onChange={(e) => setProduct({ ...product, name: e.target.value })}
+    />
+    <input
+     type="number"
+     value={product.price}
+     onChange={(e) => setProduct({ ...product, price: e.target.value })}
+    />
+    <input
+     type="text"
+     value={product.image}
+     onChange={(e) => setProduct({ ...product, image: e.target.value })}
+    />
+    <button type="submit">Add To Cart</button>
+   </form>
   </div>
  );
 };
